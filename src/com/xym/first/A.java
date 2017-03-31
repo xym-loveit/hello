91337d1c0715796836ddbef4ef2747f9c5abdf95
xym <xym_loveit@126.com>
2017-03-31 16:17:26 +0800

    Initial commit

---
+++ src/com/xym/first/A.java
@@ -0,0 +1,18 @@
+package com.xym.first;
+
+;
+
+/**
+ * aaa
+ * xym_loveit@126.com
+ *
+ * @author xym
+ * @create 2017-03-31-15:06
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+}
