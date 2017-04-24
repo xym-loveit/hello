d6aecdeedc2ac3b9d40de50e7c4403f6c0d3a2cb
xym <xym_loveit@126.com>
2017-04-24 15:31:43 +0800

    测试下

--- src/com/xym/first/A.java
+++ src/com/xym/first/A.java
@@ -10,9 +10,21 @@ package com.xym.first;
  */
 public class A {
 
+    private Student student;
+
     public static void main(String[] args) {
-        System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("-----------xym-----------值=" + args[0] + "," + "当前类=A.main()");
+        int[] aa = new int[]{1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < aa.length; i++) {
+        //    printA(aa[i]);
+        //System.out.println("~~~~~~~~~~~~~~~~~~~~~" + i);
+    }
+
+}
+
+
+    public static void printA(int xym) {
+        System.out.println("-----------xym-----------xym值=" + xym + "," + "当前类=A.printA()");
+
     }
 
 }
---
+++ src/com/xym/first/TestMain.java
@@ -0,0 +1,25 @@
+package com.xym.first;
+
+import java.util.*;
+
+/**
+ *ss
+ *xym_loveit@126.com
+ *@author xym
+ *@create 2017-04-17-18:23
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		set.add("sss");
+
+		Dictionary dictionary=new Hashtable();
+
+		dictionary.put("","dddd");
+
+
+		Map<String,String> map=new HashMap<String,String>();
+		map.put(null,null);
+	}
+
+}
